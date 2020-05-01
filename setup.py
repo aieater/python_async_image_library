e2d3fc0c2f754336f8c075cd42eeb194e8d3becc
Aki <aki.nakatsuka@pegara.com>
2020-05-01 10:30:07 +0900

    Added deps.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-version = '1.1.3'
+version = '1.1.4'
 
 install_requires = [
     'mss',
@@ -15,6 +15,7 @@ install_requires = [
     'acapture',
     'twisted',
     'tqdm',
+    'easydict',
     'service_identity',
 ]
 
